56dd311aa2a4e8110fab1c0d9962988e7eb58983
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-02-28 16:25:17 +0100

    Einbau NavigationContainer, gibt aber Laufzeitfehler in Emulator

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function EingabeSeite() { 
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hallo Kilometerumrechnung</Text>
+      <Button title="Berechnung ausführen" />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator(); // Stack-Objekt hat Attribute "Navigation" und "Screen"
 
 function App(): JSX.Element {
 
   return (
-    <View style={styles.container}>
-      <Text>Hallo Kilometerumrechnung</Text>
-      <Button title="Berechnung ausführen" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={EingabeSeite} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }; 
 
